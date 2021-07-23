d9a667db47559d3aa9dd0e9b4b49f662ba8ed6b8
tvtk <tomas.vitak@seznam.cz>
2021-07-23 12:19:53 +0200

    Enhanced to allow playbook suffixes ".yml" and return playbook result codes while exiting

--- play.sh
+++ play.sh
@@ -13,15 +13,16 @@ if [ ! -d "${LOG_DIR}/${1}" ]
 then
    mkdir -p "${LOG_DIR}/${1}"
 fi
-if [ ! -f "${2}.yml" ]
+PB_NAME=$( basename "${2}" ".yml" )
+if [ ! -f "${PB_NAME}.yml" ]
 then
-   echo "ERROR: nonexistent playbook \"${2}.yml\""
+   echo "ERROR: nonexistent playbook \"${PB_NAME}.yml\""
    echo "   Usage: ./play.sh <inventory_host_name> <playbook_name>]"
    exit 1
 fi
 
 TMSTAMP="$(date +%s)"
-LOG="logs/${1}/$(date +%Y%m%dT%H%M%S%z --date=@${TMSTAMP})_${2}.log"
+LOG="logs/${1}/$(date +%Y%m%dT%H%M%S%z --date=@${TMSTAMP})_${PB_NAME}.log"
 if [ -d ".git" ]
 then
    # record git info to the log
@@ -31,10 +32,14 @@ then
    git log -n 1 >> "${LOG}" 2>&1
    echo "#------ GIT INFO END ------" >> "${LOG}"
 fi
-ansible-playbook -vvv -l "${1}" -i hosts -e "ansible_user=ans_robot" "${2}.yml" | tee -a "${LOG}"
+ansible-playbook -vvv -l "${1}" -i hosts -e "ansible_user=ans_robot" "${PB_NAME}.yml" 2>&1 | tee -a "${LOG}"
+PB_RESULT="${PIPESTATUS[0]}"
 ENDTM="$(date +%s)"
 echo "----------------" >> "${LOG}"
 ELAPSED=$((${ENDTM} - ${TMSTAMP}))
-echo "Finished in ${ELAPSED} sec/ $(date -u +"%H:%M:%S hms" --date=@${ELAPSED})" >> "${LOG}"
+echo "Finished in ${ELAPSED} sec/$(date -u +"%H:%M:%S hms" --date=@${ELAPSED})" >> "${LOG}"
 echo "----------------" >> "${LOG}"
 echo "$(date +%Y%m%dT%H%M%S%z --date=@${ENDTM})" >> "${LOG}"
+#
+exit ${PB_RESULT}
+
